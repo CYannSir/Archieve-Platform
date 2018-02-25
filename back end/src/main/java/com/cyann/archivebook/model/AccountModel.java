0618ccefe2c4961a130dabd0ec25caf51e262212
CYann <wcy623209668@vip.qq.com>
2018-02-26 01:32:07 +0800

    update-add accountmodel

--- back end/src/main/java/com/cyann/archivebook/model/AccountModel.java
+++ back end/src/main/java/com/cyann/archivebook/model/AccountModel.java
@@ -1,14 +1,37 @@
 package com.cyann.archivebook.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 /**
  * @author CYann
  * @date 2018-02-26 0:31
+ *  户口
+ * -所属用户编号 userNum(String varchar) 外键
+ * -户口所在地 accountAddress(String varchar)
  */
 @Entity
 @Table(name ="tb_account")
 public class AccountModel extends BaseEntity{
+    @Column(nullable = false , length = 32)
+    private String userNum;
+    @Column(nullable = false , length = 64)
+    private String accountAddress;
 
+    public String getAccountAddress() {
+        return accountAddress;
+    }
+
+    public void setAccountAddress(String accountAddress) {
+        this.accountAddress = accountAddress;
+    }
+
+    public String getUserNum() {
+        return userNum;
+    }
+
+    public void setUserNum(String userNum) {
+        this.userNum = userNum;
+    }
 }
