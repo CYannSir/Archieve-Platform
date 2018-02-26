f224c42a4199d314c9f1b215e8216b1451531f8d
CYann <wcy623209668@vip.qq.com>
2018-02-26 22:24:37 +0800

    add-add userservice.java

--- back end/src/main/java/com/cyann/archivebook/service/UserService.java
+++ back end/src/main/java/com/cyann/archivebook/service/UserService.java
@@ -1,8 +1,11 @@
 package com.cyann.archivebook.service;
 
 import com.cyann.archivebook.respository.UserRespository;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 /**
  * @author CYann
@@ -12,5 +15,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserRespository userRespository;
+
+    //增加用户
+    public void add(User user){
+        userRespository.save(user);
+    }
+
+    //删除用户
+    @Transactional
+    public void delete(User user){
+
+    }
+
 
 }
